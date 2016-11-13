9eda864fe50f9aa4dcb4e8eb86433f3048bde459
Aida Benito <anbenito@yahoo.com.ar>
2016-11-13 16:19:49 -0300

    avatar palette

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -16,9 +16,9 @@ angular.module('app.controllers', [])
     })
 
 .controller('sidebarProfileController',function($scope,$window){
-  $scope.profile={picture:"",first_name:"Marcelo",last_name:"Pasut"};
-
-$scope.letters=$scope.profile.first_name.substr(0,1)+$scope.profile.last_name.substr(0,1);
+  $scope.profile={picture:"",first_name:"marcelo",last_name:"pasut"};
+  $scope.letters=($scope.profile.first_name.substr(0,1)+$scope.profile.last_name.substr(0,1)).toUpperCase();
+  $scope.color_palette=['#ba68c8','#a1887f','#058be3','#4db6ac','#e06055','#fdd835','#f6bf26','#ff8a65','#4FC3F7','#D4E157'];
 
 })
 
