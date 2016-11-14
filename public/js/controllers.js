e748730029c5512510353ef39364ff617927ab44
Aida Benito <anbenito@yahoo.com.ar>
2016-11-13 22:42:00 -0300

    formulario para modificar about

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -27,7 +27,7 @@ angular.module('app.controllers', [])
         $scope.letters=($scope.profile.first_name.substr(0,1)+$scope.profile.last_name.substr(0,1)).toUpperCase();
         $scope.prof=true;
 
-     
+
     },
     function(error) {
         $scope.errors.getProfile= true;
@@ -37,6 +37,26 @@ angular.module('app.controllers', [])
  // $scope.profile={picture:"",first_name:"marcelo",last_name:"pasut"};
 
   $scope.color_palette=['#e25f51','#f26091','#bb65ca','#9572cf','#7884cd','#5b95f9','#48c2f9','#45d0e2','#48b6ac','#52bc89','#9bce5f','#d4e34a','#feda10','#f7c000','#ffa800','#ff8a60','#c2c2c2','#8fa4af','#a2887e','#a3a3a3','#afb5e2','#b39bdd','#c2c2c2','#7cdeeb','#bcaaa4','#aed67d'];
+
+    $scope.editInf=function(){
+      $scope.editInfo=1;
+    };
+
+    $scope.cancEditInfo=function(){
+        $scope.editInfo=0;
+    };
+    $scope.editSum=function(){
+        $scope.editSummary=1;
+    };
+    $scope.cancEditSum=function(){
+        $scope.editSummary=0;
+    };
+    $scope.editCont=function(){
+        $scope.editContact=1;
+    };
+    $scope.cancEditCont=function(){
+        $scope.editContact=0;
+    };
 
     //Edit
 
