7ca2723ea5dc943dc35f4f2dd051a8f0b473d6bc
Aida Benito <anbenito@yahoo.com.ar>
2016-11-13 22:26:30 -0300

    profile detallado

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -15,13 +15,19 @@ angular.module('app.controllers', [])
 
     })
 
-.controller('sidebarProfileController',function($scope,$window,profileService){
-$scope.errors={};
+.controller('ProfileController',function($scope,profileService){
+    $scope.editSummary = 0;
+    $scope.editInfo = 0;
+    $scope.editContact = 0;
+
+    $scope.errors={};
     profileService.getProfile().then(
     function(data) {
         $scope.profile=data;
         $scope.letters=($scope.profile.first_name.substr(0,1)+$scope.profile.last_name.substr(0,1)).toUpperCase();
         $scope.prof=true;
+
+     
     },
     function(error) {
         $scope.errors.getProfile= true;
@@ -32,6 +38,27 @@ $scope.errors={};
 
   $scope.color_palette=['#e25f51','#f26091','#bb65ca','#9572cf','#7884cd','#5b95f9','#48c2f9','#45d0e2','#48b6ac','#52bc89','#9bce5f','#d4e34a','#feda10','#f7c000','#ffa800','#ff8a60','#c2c2c2','#8fa4af','#a2887e','#a3a3a3','#afb5e2','#b39bdd','#c2c2c2','#7cdeeb','#bcaaa4','#aed67d'];
 
-})
+    //Edit
 
-;
+
+    $scope.submit = function(item, message) {
+        if (item === 'profileSummary') {
+            $scope.editSummary = 0;
+        }
+
+        if (item === 'profileInfo') {
+            $scope.editInfo = 0;
+        }
+
+        if (item === 'profileContact') {
+            $scope.editContact = 0;
+        }
+
+    }
+
+
+
+});
+
+
+
--- routes/profile.js
+++ routes/profile.js
@@ -18,7 +18,7 @@ var getProfile=function(req,res){
             res.status(statusCode).send();
         } else {
             var result = JSON.parse(body);
-           // result.picture = "https://scontent-gru2-1.xx.fbcdn.net/v/t1.0-9/11150172_662840547182178_4305673317108362832_n.jpg?oh=2102aae8ab5726b46b25cbfecab1e059&oe=588E4EF9";
+         //   result.picture = "https://scontent-gru2-1.xx.fbcdn.net/v/t1.0-9/11150172_662840547182178_4305673317108362832_n.jpg?oh=2102aae8ab5726b46b25cbfecab1e059&oe=588E4EF9";
             res.send(result);
         }
 
